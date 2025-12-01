239839691421cb761c71eeac764afdf8152b31a0
Darrell-Poh <darrellpkz-sm22@student.tarc.edu.my>
2025-12-01 16:29:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.cdo6qemis4wa.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "pokhaizhen-employee"
+custombucket = "pohkhaizhen-employee"
 customregion = "us-east-1"
 
