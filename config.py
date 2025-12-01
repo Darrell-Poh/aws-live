8dce30b6732ff2e7690eced8d84ce579dbb43763
Darrell-Poh <darrellpkz-sm22@student.tarc.edu.my>
2025-12-01 16:06:28 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cdo6qemis4wa.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "pokhaizhen-employee"
+customregion = "us-east-1"
+
